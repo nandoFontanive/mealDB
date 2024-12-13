5b1c9e0497617ba5379e4fa877bf1131fff7ac10
Fernando Fontanive <nando.fontanive@gmail.com>
2024-12-13 13:28:40 -0300

    Implemented separate views, toolbar ok
    Toolbar with opaque white background

---
+++ mealDB/mealDB/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  mealDB
+//
+//  Created by Fernando Fontanive on 13/12/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text("abc")
+    }
+}
+
+#Preview {
+    MainTabView()
+}
